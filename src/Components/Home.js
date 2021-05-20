9ef33ad0ea8b0372279041e14f34fd5faad14c3b
thephotogenicbug <navinkumarachari@gmail.com>
2021-05-21 00:20:53 +0530

    website based on ReactJS

---
+++ src/Components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Home.css';
+import banner from './banner.png'
+const Home = () =>{
+
+    return(
+        <div className="container mt-3" >
+            <div className="row">
+                <div className="col-md-6 "id="Banner">
+                    <h1 ><span className="heading">Website </span> & <span className="heading">Web Apps</span></h1>
+                    <h2 className="heading2">Development</h2>
+                    <p>We Build Responsive website and web apps for at affordable price</p>
+                    <button className="btn text-white mt-2" id="btn">Learn More</button>
+                    <button className="btn mt-2" id="btn1">Contact US</button>
+                </div>
+                <div className="col-md-6">
+                    <img src={banner} className="img-fluid" />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home;
