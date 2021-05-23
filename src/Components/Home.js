49a8d9ae2c3836ddea0f0d721ef613d7933510e3
thephotogenicbug <navinkumarachari@gmail.com>
2021-05-23 19:16:34 +0530

    React-website: update Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -49,50 +49,14 @@ const Home = () =>{
                       </ul>
                 </div>
             </div>
-            <div className="row mt-4">
-                <div className="col-md-12 text-center">
-                    <h4>Benefits of working with us</h4>
-                </div>
-            </div>
-            <div className="row mt-3 ">
-            <div className="col-md-4" id="cards">
-                    <div className="card">
-                      <div className="card-body bg-primary text-white text-center">
-                           <label >RESPONSIVE DESIGN</label> 
-                           <br/><br/>
-                          Fully responsive website to reach every device audience
-                      </div>
-                    </div> 
-                </div>
-                <div className="col-md-4" id="cards">
-                    <div className="card">
-                      <div className="card-body bg-danger text-white text-center">
-                           <label>SEO FRIENDLY</label> 
-                           <br/><br/>
-                           Optimized website for easy search engine ranking
-                      </div>
-                    </div>
-                </div>
-                <div className="col-md-4" id="cards">
-                    <div className="card">
-                      <div className="card-body bg-dark text-white text-center">
-                           <label>AFFORDABLE</label> 
-                           <br/><br/>
-                           Economic ease for starter business with less load on pockets
-                      </div>
-                    </div>
-                </div>
-                <div className="col-md-4" id="cards">
-                    <div className="card">
-                      <div className="card-body bg-success text-white  text-center">
-                           <label >FULLY CUSTOMIZED</label> 
-                           <br/><br/>
-                           A tailored layout which matches your business and user.
-                      </div>
-                    </div>
-                </div>
+            <div className="row mt-3" >
+             <div className="col-md-12 text-center">
+             <h3 >REQUEST A CALLBACK</h3>
+             </div>
+            
             </div>
         </div>
+        
     )
 }
 export default Home;
---
+++ src/Components/test.js
@@ -0,0 +1,38 @@
+<div className="row mt-3 ">
+<div className="col-md-4" id="cards">
+        <div className="card">
+          <div className="card-body bg-primary text-white text-center">
+               <label >RESPONSIVE DESIGN</label> 
+               <br/><br/>
+              Fully responsive website to reach every device audience
+          </div>
+        </div> 
+    </div>
+    <div className="col-md-4" id="cards">
+        <div className="card">
+          <div className="card-body bg-danger text-white text-center">
+               <label>SEO FRIENDLY</label> 
+               <br/><br/>
+               Optimized website for easy search engine ranking
+          </div>
+        </div>
+    </div>
+    <div className="col-md-4" id="cards">
+        <div className="card">
+          <div className="card-body bg-dark text-white text-center">
+               <label>AFFORDABLE</label> 
+               <br/><br/>
+               Economic ease for starter business with less load on pockets
+          </div>
+        </div>
+    </div>
+    <div className="col-md-4" id="cards">
+        <div className="card">
+          <div className="card-body bg-success text-white  text-center">
+               <label >FULLY CUSTOMIZED</label> 
+               <br/><br/>
+               A tailored layout which matches your business and user.
+          </div>
+        </div>
+    </div>
+</div>
